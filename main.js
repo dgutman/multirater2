36b0348a5e07a3deb0bf17e6ec79fa6c58b8eea4
Aadi K <aadi.kalloo@gmail.com>
2018-08-21 20:08:04 -0600

    New Multirater

---
+++ main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  var svg = d3.select("body")
+    .append("svg")
+    .attr("id", "main_svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .call(d3.zoom().on("zoom", function () {
+            svg.attr("transform", d3.event.transform)
+    }))
+    .append("g")
+
+  svg.append("svg:image")
+    .attr("xlink:href", "https://isic-archive.com/api/v1/image/558d6301bae47801cf734ad1/download?contentDisposition=inline")
+
+})
